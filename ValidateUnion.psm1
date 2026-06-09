fbd57695f29f8bc9ab58a720d56fad7d4e5d24d6
vike <vike2000@gmail.com>
2026-06-09 14:09:41 +0200

    v0.2.0

--- ValidateUnion.psm1
+++ ValidateUnion.psm1
@@ -1,7 +1,7 @@
 
 <#PSScriptInfo
 
-.VERSION 0.1.3
+.VERSION 0.2
 
 .GUID 0a96e04b-b5ee-4752-a446-8d43e2db67dc
 
@@ -112,9 +112,23 @@
 				,																		$sixth		`
 				,																		$seventh	`
 				,																		$eighth		)	} #n=8
-		 [void		]								 Validate				([object]	$arguments		`
-			,[System.Management.Automation.EngineIntrinsics]							$engineIntrinsics)
-			{			foreach($arg	in												$arguments	)
+		 [void		]								 Validate				([object]	$argument		
+		 	,[System.Management.Automation.EngineIntrinsics]							$engineIntrinsics)
+			{<#
+#			;Write-Host "STACK:"
+#			;Get-PSCallStack | Select-Object -First 8 | Format-Table -AutoSize | Out-Host
+#			;Write-Host "VALIDATING:"
+#			;Write-Host "Type: $($arguments.GetType().FullName)"
+#			;Write-Host "Value: <$arguments>"
+#			;if	($arguments -is [System.Array])
+#				{Write-Host "Array count: $($arguments.Count)"}
+#			;Write-Host "----"
+#			;Write-Host "Attribute instance: $($this.GetHashCode())"
+#			;Write-Host "Types:"
+#			;$this.Types | % { Write-Host "  $_" }
+#			;Write-Host "Argument type: $($arguments.GetType().FullName)"
+#			;Write-Host "Argument value: <$arguments>"
+			;			foreach($arg	in												$arguments	)
 				{				$val = $false														;
 				;		foreach($typ	in	$this.	 Types										  )	`
 				  {	if(			$arg															`
@@ -122,10 +136,22 @@
 					{			$val = $true													;
 					;break																		} }
 				;		if(-not	$val															  )
-				  {throw						   [ValidateUnionException]::new("Type"			`
+				  {throw						   [ValidateUnionException]::new("VU[$($this.GetHashCode())] " + "Type"			`
 					+" "+				((	$this.	 Types|foreach-object {"[$_]"}) -join ', ')	`
 					+" "+		"required"												+ ","	`
 					+" "+		$arg.getType()													`
 					+" "+		"given"															`
 					+" "+		"in value:"														`
-					+" "+		$arg															) }	}	}	}
+					+" "+		$arg															) }	} #>
+			;foreach(								$type
+				in							$this.	 Types											)
+				{if	(			$argument -is		$type										)	`
+					{return																		}	}	;
+			;throw							   [ValidateUnionException]::new("Type"					`
+				+" "+				((	$this.	 Types|foreach-object {"[$_]"}) -join ', ')			`
+				+" "+			"required"											+ ","			`
+				+" "+			$argument.getType()													`
+				+" "+			"given"																`
+				+" "+			"in value:"															`
+				+" "+			$argument															)	;
+																										}	}
--- tests/ValidateUnion.pester.ps1
+++ tests/ValidateUnion.pester.ps1
@@ -1,16 +1,32 @@
-beforeAll {
-	function fixture([ValidateUnion([scriptBlock], [string])] $argument) {}
-}
-
-describe "ValidateUnion" {
-	it "given a valid argument" {
-		{fixture 'test'	}|should -not	-throw
-		{fixture {test}	}|should -not	-throw
-	}
-	it "given an invalid argument" {
-		{fixture $true	}|should		-throw -expectedMessage "Cannot validate argument on parameter 'argument'. Type ``[scriptblock``], ``[string``] required, bool $(	)given in value: True"
-		{fixture 1		}|should		-throw -expectedMessage "Cannot validate argument on parameter 'argument'. Type ``[scriptblock``], ``[string``] required, int $(	)given in value: 1"
-	}
-}
-
-# vike's personal execution: cd1 /œ∂/ps1/com.dyn_o_saur.bluelephant.pwsh/ValidateUnion invoke-isolated -verbose -usingPathModule ./ValidateUnion.psd1 {invoke-pester tests/*}
+	# verbatim from vike's personal base.ps1
+	# return arguments, being an array, not having to quote strings or use commas
+	# example:
+	# &{$arr=@(arrgs a m z);$arr}
+	function arrgs		{	if ($args.length -le 0)	{@(								 )}
+						elseif ($args.length -le 1)	{@(	$args[0						])}
+						else						{	$args[0..$args.length		] }} #cspell:words arrgs
+	
+	# support <specified> name template
+	;function			s(){process{$_.specified="of $($_.s[2]) it should …";$_}}
+	
+	;beforeAll {
+		function acceptingScriptBlockOrString(	[ValidateUnion([scriptBlock], [string	])] $argument){}
+		function acceptingScriptBlockOrArray(	[ValidateUnion([scriptBlock], [array	])] $argument){}}
+	
+	;describe "ValidateUnion"																																																									`
+		{it "given a valid argument$(	)<specified>"																																																-foreach	`
+			(  @(	@{	 s=@(arrgs acceptingScriptBlockOrString	{test}	should -not	-throw																																							)}	`
+			,  		@{	 s=@(arrgs acceptingScriptBlockOrString	'test'	should -not	-throw																																							)}	`
+			,  		@{	 s=@(arrgs acceptingScriptBlockOrArray	{test}	should -not	-throw																																							)}	`
+			,  		@{	 s=@(arrgs acceptingScriptBlockOrArray   @(	 )	should -not	-throw																																							)}	)|s	)	`
+			{																	$a=$s[3..$s.count]																																							;
+			;	{	&	$s[0]								$s[1]}|		should	@a																																											}	;
+		;it "given an invalid argument$(	)<specified>"																																															-foreach	`
+			(  @(	@{	 s=@(arrgs acceptingScriptBlockOrString	 1		should		-throw -expectedMessage "Cannot validate argument on parameter 'argument'. Type ``[scriptblock``], ``[string$(	)``] required, int $(	)given in value: 1"		)}	`
+			,  		@{	 s=@(arrgs acceptingScriptBlockOrString	$true	should		-throw -expectedMessage "Cannot validate argument on parameter 'argument'. Type ``[scriptblock``], ``[string$(	)``] required, bool $(	)given in value: True"	)}	`
+			,  		@{	 s=@(arrgs acceptingScriptBlockOrArray	$true	should		-throw -expectedMessage "Cannot validate argument on parameter 'argument'. Type ``[scriptblock``], ``[array$(	)``] required, bool $(	)given in value: True"	)}	`
+			,  		@{	 s=@(arrgs acceptingScriptBlockOrArray	 1		should		-throw -expectedMessage "Cannot validate argument on parameter 'argument'. Type ``[scriptblock``], ``[array$(	)``] required, int $(	)given in value: 1"		)}	)|s	)	`
+			{																	$a=$s[3..$s.count]																																							;
+			;	{	&	$s[0]								$s[1]}|		should	@a																																											}	}
+	
+	# vike's personal execution: cd1 /œ∂/ps1/com.dyn_o_saur.bluelephant.pwsh/ValidateUnion invoke-isolated -verbose -usingPathModule ./ValidateUnion.psd1 {invoke-pester tests/*}

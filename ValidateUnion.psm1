d364080676f8e9e1c767bb9631e6a58ba967bad9
vike <vike2000@gmail.com>
2026-07-18 12:56:10 +0200

    v0.3.0 - adding ValidateUnionArrayItemsAttribute for validating array (enumerable) items types

--- ValidateUnion.psm1
+++ ValidateUnion.psm1
@@ -26,103 +26,215 @@
 
 #>
 
-	class											 ValidateUnionException:System.Runtime.InteropServices.ExternalException
-		{											 ValidateUnionException	($message)
-		:															base	($message){}}
-	class											 ValidateUnionAttribute
-	:					System.Management.Automation.ValidateArgumentsAttribute
-		{[object[]	]								$Types													;
-		 											 ValidateUnionAttribute									`
-			([type]																		$single			)
-			{								$this.	 Types					=@(			$single		)	} # n=1
-		 											 ValidateUnionAttribute									`
-			([type]																		$first			`
-			,[type]																		$second			)
-			{								$this.	 Types					=@(			$first		`
-				,																		$second		)	} # n=2
-		 											 ValidateUnionAttribute	`
-			([type]																		$first			`
-			,[type]																		$second			`
-			,[type]																		$third			)
-			{								$this.	 Types					=@(			$first		`
-				,																		$second		`
-				,																		$third		)	} # n=3
-		 											 ValidateUnionAttribute	`
-			([type]																		$first			`
-			,[type]																		$second			`
-			,[type]																		$third			`
-			,[type]																		$fourth			)
-			{								$this.	 Types					=@(			$first		`
-				,																		$second		`
-				,																		$third		`
-				,																		$fourth		)	} # n=4
-		 											 ValidateUnionAttribute	`
-			([type]																		$first			`
-			,[type]																		$second			`
-			,[type]																		$third			`
-			,[type]																		$fourth			`
-			,[type]																		$fifth			)
-			{								$this.	 Types					=@(			$first		`
-				,																		$second		`
-				,																		$third		`
-				,																		$fourth		`
-				,																		$fifth		)	} #n=5
-		 											 ValidateUnionAttribute	`
-			([type]																		$first			`
-			,[type]																		$second			`
-			,[type]																		$third			`
-			,[type]																		$fourth			`
-			,[type]																		$fifth			`
-			,[type]																		$sixth			)
-			{								$this.	 Types					=@(			$first		`
-				,																		$second		`
-				,																		$third		`
-				,																		$fourth		`
-				,																		$fifth		`
-				,																		$sixth		)	} #n=6
-		 											 ValidateUnionAttribute	`
-			([type]																		$first			`
-			,[type]																		$second			`
-			,[type]																		$third			`
-			,[type]																		$fourth			`
-			,[type]																		$fifth			`
-			,[type]																		$sixth			`
-			,[type]																		$seventh		)
-			{								$this.	 Types					=@(			$first		`
-				,																		$second		`
-				,																		$third		`
-				,																		$fourth		`
-				,																		$fifth		`
-				,																		$sixth		`
-				,																		$seventh	)	} #n=7
-		 											 ValidateUnionAttribute	`
-			([type]																		$first			`
-			,[type]																		$second			`
-			,[type]																		$third			`
-			,[type]																		$fourth			`
-			,[type]																		$fifth			`
-			,[type]																		$sixth			`
-			,[type]																		$seventh		`
-				,																		$eighth			)
-			{								$this.	 Types					=@(			$first		`
-				,																		$second		`
-				,																		$third		`
-				,																		$fourth		`
-				,																		$fifth		`
-				,																		$sixth		`
-				,																		$seventh	`
-				,																		$eighth		)	} #n=8
-		 [void		]								 Validate				([object]	$argument		
-		 	,[System.Management.Automation.EngineIntrinsics]							$engineIntrinsics)
-			{foreach(								$type
-				in							$this.	 Types											)
-				{if	(			$argument -is		$type										)	`
-					{return																		}	}	;
-			;throw							   [ValidateUnionException]::new("Type"					`
-				+" "+				((	$this.	 Types|foreach-object {"[$_]"}) -join ', ')			`
-				+" "+			"required"											+ ","			`
-				+" "+			$argument.getType()													`
-				+" "+			"given"																`
-				+" "+			"in value:"															`
-				+" "+			$argument															)	;
-																										}	}
+#cspell:ignore COMPANYNAME LICENSEURI PROJECTURI
+
+	class												 ValidateUnionException:System.Runtime.InteropServices.ExternalException
+		{												 ValidateUnionException	($message)
+		:																base	($message){}}
+	class												 ValidateUnionAttribute
+	:						System.Management.Automation.ValidateArgumentsAttribute
+		{	 [type[]]									$Types														;
+		 												 ValidateUnionAttribute										`
+			([type]																			$single				)
+			{									$this.	 Types					=@(			$single			)	} # n=1
+		 												 ValidateUnionAttribute										`
+			([type]																			$first				`
+			,[type]																			$second				)
+			{									$this.	 Types					=@(			$first			`
+				,																			$second			)	} # n=2
+		 												 ValidateUnionAttribute										`
+			([type]																			$first				`
+			,[type]																			$second				`
+			,[type]																			$third				)
+			{									$this.	 Types					=@(			$first			`
+				,																			$second			`
+				,																			$third			)	} # n=3
+		 												 ValidateUnionAttribute										`
+			([type]																			$first				`
+			,[type]																			$second				`
+			,[type]																			$third				`
+			,[type]																			$fourth				)
+			{									$this.	 Types					=@(			$first			`
+				,																			$second			`
+				,																			$third			`
+				,																			$fourth			)	} # n=4
+		 												 ValidateUnionAttribute										`
+			([type]																			$first				`
+			,[type]																			$second				`
+			,[type]																			$third				`
+			,[type]																			$fourth				`
+			,[type]																			$fifth				)
+			{									$this.	 Types					=@(			$first			`
+				,																			$second			`
+				,																			$third			`
+				,																			$fourth			`
+				,																			$fifth			)	} #n=5
+		 												 ValidateUnionAttribute										`
+			([type]																			$first				`
+			,[type]																			$second				`
+			,[type]																			$third				`
+			,[type]																			$fourth				`
+			,[type]																			$fifth				`
+			,[type]																			$sixth				)
+			{									$this.	 Types					=@(			$first			`
+				,																			$second			`
+				,																			$third			`
+				,																			$fourth			`
+				,																			$fifth			`
+				,																			$sixth			)	} #n=6
+		 												 ValidateUnionAttribute										`
+			([type]																			$first				`
+			,[type]																			$second				`
+			,[type]																			$third				`
+			,[type]																			$fourth				`
+			,[type]																			$fifth				`
+			,[type]																			$sixth				`
+			,[type]																			$seventh			)
+			{									$this.	 Types					=@(			$first			`
+				,																			$second			`
+				,																			$third			`
+				,																			$fourth			`
+				,																			$fifth			`
+				,																			$sixth			`
+				,																			$seventh		)	} #n=7
+		 												 ValidateUnionAttribute										`
+			([type]																			$first				`
+			,[type]																			$second				`
+			,[type]																			$third				`
+			,[type]																			$fourth				`
+			,[type]																			$fifth				`
+			,[type]																			$sixth				`
+			,[type]																			$seventh			`
+				,																			$eighth				)
+			{									$this.	 Types					=@(			$first			`
+				,																			$second			`
+				,																			$third			`
+				,																			$fourth			`
+				,																			$fifth			`
+				,																			$sixth			`
+				,																			$seventh		`
+				,																			$eighth			)	} #n=8
+		 [void		]									 Validate				([object]	$argument			`
+		 	,[System.Management.Automation.EngineIntrinsics]								$engineIntrinsics	)
+			{foreach(									$type												`
+				in								$this.	 Types												)
+				{if	(				$argument	-is		$type											)	`
+					{return																				}	}	;
+			;throw								   [ValidateUnionException]::new("Type"						`
+				+" "+						((	$this.	 Types|foreach-object {"[$_]"})		-join ', ')		`
+				+" "+				"required"													+ ","		`
+				+" "+				$argument.		  getType()												`
+				+" "+				"given"																	`
+				+" "+				"in value:"																`
+				+" "+				$argument																)	}	}
+	class												 ValidateUnionArrayItemsAttribute
+	:						System.Management.Automation.ValidateArgumentsAttribute
+		{	 [type[]]									$Types														;
+		 												 ValidateUnionArrayItemsAttribute							`
+			([type]																			$single				)
+			{									$this.	 Types					=@(			$single			)	} # n=1
+		 												 ValidateUnionArrayItemsAttribute							`
+			([type]																			$first				`
+			,[type]																			$second				)
+			{									$this.	 Types					=@(			$first			`
+				,																			$second			)	} # n=2
+		 												 ValidateUnionArrayItemsAttribute							`
+			([type]																			$first				`
+			,[type]																			$second				`
+			,[type]																			$third				)
+			{									$this.	 Types					=@(			$first			`
+				,																			$second			`
+				,																			$third			)	} # n=3
+		 												 ValidateUnionArrayItemsAttribute							`
+			([type]																			$first				`
+			,[type]																			$second				`
+			,[type]																			$third				`
+			,[type]																			$fourth				)
+			{									$this.	 Types					=@(			$first			`
+				,																			$second			`
+				,																			$third			`
+				,																			$fourth			)	} # n=4
+		 												 ValidateUnionArrayItemsAttribute							`
+			([type]																			$first				`
+			,[type]																			$second				`
+			,[type]																			$third				`
+			,[type]																			$fourth				`
+			,[type]																			$fifth				)
+			{									$this.	 Types					=@(			$first			`
+				,																			$second			`
+				,																			$third			`
+				,																			$fourth			`
+				,																			$fifth			)	} #n=5
+		 												 ValidateUnionArrayItemsAttribute							`
+			([type]																			$first				`
+			,[type]																			$second				`
+			,[type]																			$third				`
+			,[type]																			$fourth				`
+			,[type]																			$fifth				`
+			,[type]																			$sixth				)
+			{									$this.	 Types					=@(			$first			`
+				,																			$second			`
+				,																			$third			`
+				,																			$fourth			`
+				,																			$fifth			`
+				,																			$sixth			)	} #n=6
+		 												 ValidateUnionArrayItemsAttribute							`
+			([type]																			$first				`
+			,[type]																			$second				`
+			,[type]																			$third				`
+			,[type]																			$fourth				`
+			,[type]																			$fifth				`
+			,[type]																			$sixth				`
+			,[type]																			$seventh			)
+			{									$this.	 Types					=@(			$first			`
+				,																			$second			`
+				,																			$third			`
+				,																			$fourth			`
+				,																			$fifth			`
+				,																			$sixth			`
+				,																			$seventh		)	} #n=7
+		 												 ValidateUnionArrayItemsAttribute							`
+			([type]																			$first				`
+			,[type]																			$second				`
+			,[type]																			$third				`
+			,[type]																			$fourth				`
+			,[type]																			$fifth				`
+			,[type]																			$sixth				`
+			,[type]																			$seventh			`
+				,																			$eighth				)
+			{									$this.	 Types					=@(			$first			`
+				,																			$second			`
+				,																			$third			`
+				,																			$fourth			`
+				,																			$fifth			`
+				,																			$sixth			`
+				,																			$seventh		`
+				,																			$eighth			)	} #n=8
+		 [void		]									 Validate				([object]	$argument			`
+		 	,[System.Management.Automation.EngineIntrinsics]								$engineIntrinsics	)
+			{if	(																			$argument		`
+				-is																			[object[]]		`
+				-and																0	-eq	$argument.count	)	`
+				{			return																			}	;
+			;						$item_serial=0																;
+			;						$valid = $false																;
+			;		foreach(		$item in $argument														)	`
+				{					$valid = $false															;
+				;					$item_serial++															;
+				;	foreach(							$type											`
+					in							$this.	 Types											)
+					{if	(			$item		-is		$type										)	`
+						{			$valid = $true													;
+						;	break																	}	}	;
+				;if	(			!	$valid																)	`
+					{		break																		}	}	;
+			;if	(				!	$valid																	)	`
+				{throw						   [ValidateUnionException]::new("Type"						`
+					+" "+					((	$this.	 Types|foreach-object {"[$_]"})		-join ', ')	`
+					+" "+			"required"													+ ","	`
+					+" "+			$item.			  getType()											`
+					+" "+			"given"																`
+					+" "+			"in item number"													`
+					+" "+			$item_serial														`
+					+" "+"with"+" "+   "value:"															`
+					+" "+			$item																)	}	}	}
--- tests/ValidateUnion.pester.ps1
+++ tests/ValidateUnion.pester.ps1
@@ -7,26 +7,44 @@
 						else						{	$args[0..$args.length		] }} #cspell:words arrgs
 	
 	# support <specified> name template
-	;function			s(){process{$_.specified="of $($_.s[2]) it should …";$_}}
+	;function			s(){process{$_.specified=" of $(convertTo-psCode ($_.s[1])) it should $($_.s[3..$_.s.count]|foreach-object{$_ -replace '^-',''})";$_}}
 	
 	;beforeAll {
-		function acceptingScriptBlockOrString(	[ValidateUnion([scriptBlock], [string	])] $argument){}
-		function acceptingScriptBlockOrArray(	[ValidateUnion([scriptBlock], [array	])] $argument){}}
+		function acceptingScriptBlockOrString(			[ValidateUnion(				[scriptBlock], [string	])] $argument){}
+		function acceptingScriptBlockOrArray(			[ValidateUnion(				[scriptBlock], [array	])] $argument){}
+		function acceptingArrayOfScriptBlockOrString(	[ValidateUnionArrayItems(	[scriptBlock], [string	])] $argument){}
+		function acceptingArrayOfScriptBlockOrArray(	[ValidateUnionArrayItems(	[scriptBlock], [array	])] $argument){}}
 	
-	;describe "ValidateUnion"																																																									`
-		{it "given a valid argument$(	)<specified>"																																																-foreach	`
-			(  @(	@{	 s=@(arrgs acceptingScriptBlockOrString	{test}	should -not	-throw																																							)}	`
-			,  		@{	 s=@(arrgs acceptingScriptBlockOrString	'test'	should -not	-throw																																							)}	`
-			,  		@{	 s=@(arrgs acceptingScriptBlockOrArray	{test}	should -not	-throw																																							)}	`
-			,  		@{	 s=@(arrgs acceptingScriptBlockOrArray   @(	 )	should -not	-throw																																							)}	)|s	)	`
-			{																	$a=$s[3..$s.count]																																							;
-			;	{	&	$s[0]								$s[1]}|		should	@a																																											}	;
-		;it "given an invalid argument$(	)<specified>"																																															-foreach	`
-			(  @(	@{	 s=@(arrgs acceptingScriptBlockOrString	 1		should		-throw -expectedMessage "Cannot validate argument on parameter 'argument'. Type ``[scriptblock``], ``[string$(	)``] required, int $(	)given in value: 1"		)}	`
-			,  		@{	 s=@(arrgs acceptingScriptBlockOrString	$true	should		-throw -expectedMessage "Cannot validate argument on parameter 'argument'. Type ``[scriptblock``], ``[string$(	)``] required, bool $(	)given in value: True"	)}	`
-			,  		@{	 s=@(arrgs acceptingScriptBlockOrArray	$true	should		-throw -expectedMessage "Cannot validate argument on parameter 'argument'. Type ``[scriptblock``], ``[array$(	)``] required, bool $(	)given in value: True"	)}	`
-			,  		@{	 s=@(arrgs acceptingScriptBlockOrArray	 1		should		-throw -expectedMessage "Cannot validate argument on parameter 'argument'. Type ``[scriptblock``], ``[array$(	)``] required, int $(	)given in value: 1"		)}	)|s	)	`
-			{																	$a=$s[3..$s.count]																																							;
-			;	{	&	$s[0]								$s[1]}|		should	@a																																											}	}
+	;describe "ValidateUnion"																																																																		`
+		{it "given a valid argument$(	)<specified>"																																																									-foreach	`
+			(  @(	@{	 s=@(arrgs acceptingScriptBlockOrString			{test}			should -not	-throw																																												)}	`
+			,  		@{	 s=@(arrgs acceptingScriptBlockOrString			'test'			should -not	-throw																																												)}	`
+			,  		@{	 s=@(arrgs acceptingScriptBlockOrArray			{test}			should -not	-throw																																												)}	`
+			,  		@{	 s=@(arrgs acceptingScriptBlockOrArray			 @(	 )			should -not	-throw																																												)}	)|s	)	`
+			{																			$a=$s[3..$s.count]																																												;
+			;	{	&	$s[0]											$s[1]}|			should	@a																																																}	;
+		;it "given an invalid argument$(	)<specified>"																																																								-foreach	`
+			(  @(	@{	 s=@(arrgs acceptingScriptBlockOrString					 1		should		-throw -expectedMessage "Cannot validate argument on parameter 'argument'. Type ``[scriptblock``], ``[string$(	)``] required, int $(	)given in value: 1"							)}	`
+			,  		@{	 s=@(arrgs acceptingScriptBlockOrString					$true	should		-throw -expectedMessage "Cannot validate argument on parameter 'argument'. Type ``[scriptblock``], ``[string$(	)``] required, bool $(	)given in value: True"						)}	`
+			,  		@{	 s=@(arrgs acceptingScriptBlockOrArray					$true	should		-throw -expectedMessage "Cannot validate argument on parameter 'argument'. Type ``[scriptblock``], ``[array$(	)``] required, bool $(	)given in value: True"						)}	`
+			,  		@{	 s=@(arrgs acceptingScriptBlockOrArray					 1		should		-throw -expectedMessage "Cannot validate argument on parameter 'argument'. Type ``[scriptblock``], ``[array$(	)``] required, int $(	)given in value: 1"							)}	)|s	)	`
+			{																			$a=$s[3..$s.count]																																												;
+			;	{	&	$s[0]											$s[1]}|			should	@a																																																}	}
+	
+	;describe "ValidateUnionArrayItems"																																																																		`
+		{it "given a valid argument$(		)<specified>"																																																								-foreach	`
+			(  @(	@{	 s=@(arrgs acceptingArrayOfScriptBlockOrString	{test},'text'	should -not	-throw																																												)}	`
+			,  		@{	 s=@(arrgs acceptingArrayOfScriptBlockOrString	'test',{text}	should -not	-throw																																												)}	`
+			,  		@{	 s=@(arrgs acceptingArrayOfScriptBlockOrArray	{test},@(2,8)	should -not	-throw																																												)}	`
+			,  		@{	 s=@(arrgs acceptingArrayOfScriptBlockOrArray   @(	 )			should -not	-throw																																												)}	)|s	)	`
+			{																					$a=$s[3..$s.count]																																												;
+			;	{	&	$s[0]											$s[1]}|			should	@a																																																}	;
+		;it "given an invalid argument$(	)<specified>"																																																								-foreach	`
+			(  @(	@{	 s=@(arrgs acceptingArrayOfScriptBlockOrString	{test},	 1		should		-throw -expectedMessage "Cannot validate argument on parameter 'argument'. Type ``[scriptblock``], ``[string$(	)``] required, int $(	)given in item number 2 with value: 1"		)}	`
+			,  		@{	 s=@(arrgs acceptingArrayOfScriptBlockOrString	'test',	$true	should		-throw -expectedMessage "Cannot validate argument on parameter 'argument'. Type ``[scriptblock``], ``[string$(	)``] required, bool $(	)given in item number 2 with value: True"	)}	`
+			,  		@{	 s=@(arrgs acceptingArrayOfScriptBlockOrArray	{test},	$true	should		-throw -expectedMessage "Cannot validate argument on parameter 'argument'. Type ``[scriptblock``], ``[array$(	)``] required, bool $(	)given in item number 2 with value: True"	)}	`
+			,  		@{	 s=@(arrgs acceptingArrayOfScriptBlockOrArray	@(2,8),	 1		should		-throw -expectedMessage "Cannot validate argument on parameter 'argument'. Type ``[scriptblock``], ``[array$(	)``] required, int $(	)given in item number 2 with value: 1"		)}	)|s	)	`
+			{																					$a=$s[3..$s.count]																																												;
+			;	{	&	$s[0]											$s[1]}|			should	@a																																																}	}
 	
 	# vike's personal execution: cd1 /œ∂/ps1/com.dyn_o_saur.bluelephant.pwsh/ValidateUnion invoke-isolated -verbose -usingPathModule ./ValidateUnion.psd1 {invoke-pester tests/*}
